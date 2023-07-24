b5d718931965bf7d49b1ef7a57be978c46eb4ab6
GaguliChamathkara <136616090+GaguliChamathkara@users.noreply.github.com>
2023-07-24 12:12:56 +0530

    Add files via upload

---
+++ labexercise/Labexercise.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.labexercise;
+
+
+public class Labexercise {
+
+    public static void main(String[] args) {
+        test myClass = new test();
+        myClass.setVal("Amal");
+        
+        System.out.println(myClass.getVal());
+    }
+}
---
+++ labexercise/test.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.labexercise;
+
+
+public class test {
+    private String name1;
+    private int age1;
+    private int salary1;
+    public String getVal()
+    {
+        return name1;
+    }
+    public void setVal(String newName1)
+    {
+      this.name1 = newName1;  
+    }
+    
+}
+
